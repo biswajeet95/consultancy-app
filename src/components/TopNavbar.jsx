c02cdc8e2c7754846124042442068d89c1921c71
Biswajeet moharana <bmoharana95@gmail.com>
2024-06-08 03:33:26 +0530

    design

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <Toolbar variant="dense" sx={{ justifyContent: 'space-between', minHeight: '30px !important', padding: '3px 6px' }}>
                 <Box>
                     <Typography sx={{ fontWeight: 'bold' }} variant={isMobile ? 'body2' : 'body1'}>
-                        Call:: 7075910637, 9676311306
+                        Call: 7075910637, 9676311306
                     </Typography>
                 </Box>
                 <Box>
