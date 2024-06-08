7e562e9890c9acc169d9ab499b56365792ebbd5a
Biswajeet moharana <bmoharana95@gmail.com>
2024-06-08 23:29:06 +0530

    design

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -18,12 +18,12 @@ const Navbar = () => {
             <Toolbar variant="dense" sx={{ justifyContent: 'space-between', minHeight: '30px !important', padding: '3px 6px' }}>
                 <Box>
                     <Typography sx={{ fontWeight: 'bold' }} variant={isMobile ? 'body2' : 'body1'}>
-                        Call: 7075910637, 9676311306
+                        Call: 7075910637, 9154311306
                     </Typography>
                 </Box>
                 <Box>
                     <Typography sx={{ fontWeight: 'bold' }} variant={isMobile ? 'body2' : 'body1'}>
-                        admin@iqraconsultancy.in, arbazarmaan37@gmail.com
+                        admin@iqraconsultancy.in, shaikbasheer0709@gmail.com
                     </Typography>
                 </Box>
 
