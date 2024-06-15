8f574311448e9a6663081d9e7bdb871b8569ca73
Biswajeet moharana <bmoharana95@gmail.com>
2024-06-15 17:23:36 +0530

    design

--- src/components/EmailButton.jsx
+++ src/components/EmailButton.jsx
@@ -1,13 +1,47 @@
+// import React from 'react';
+// import { Button } from '@mui/material';
+// import IosShareIcon from '@mui/icons-material/IosShare';
+
+// const EmailButton = ({ fontName, font }) => {
+//   const emailAddress = "arbazarmaan37@gmail.com";
+
+//   const handleClick = () => {
+//     const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${emailAddress}`;
+//     window.open(gmailUrl, '_blank');
+//   };
+
+//   return (
+//     <Button
+//       variant="contained"
+//       sx={{
+//         backgroundColor: '#254163', // Custom background color
+//         color: '#FFFFFF', // Custom text color
+//         fontSize: font,
+//         fontWeight: 'bold',
+//         '&:hover': {
+//           backgroundColor: 'blue' // Custom hover color
+//         }
+//       }}
+//       startIcon={<IosShareIcon />}
+//       onClick={handleClick}
+//     >
+//        {fontName}
+//     </Button>
+//   );
+// };
+
+// export default EmailButton;
+
+
 import React from 'react';
 import { Button } from '@mui/material';
 import IosShareIcon from '@mui/icons-material/IosShare';
 
 const EmailButton = ({ fontName, font }) => {
-  const emailAddress = "arbazarmaan37@gmail.com";
+  const googleFormUrl = "https://docs.google.com/forms/d/e/1FAIpQLSe06ZPzCnm9uPzZO4IuewqoWm_rMwLhTJw81fSuRkO9JNH6LA/viewform";
 
   const handleClick = () => {
-    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${emailAddress}`;
-    window.open(gmailUrl, '_blank');
+    window.open(googleFormUrl, '_blank');
   };
 
   return (
@@ -25,15 +59,9 @@ const EmailButton = ({ fontName, font }) => {
       startIcon={<IosShareIcon />}
       onClick={handleClick}
     >
-       {fontName}
+      {fontName}
     </Button>
   );
 };
 
 export default EmailButton;
-
-
-
-
-
-
